369a14068de4a6fa84146dab5808ce33bd6d13db
OlgaAlekhina <olgaalekhina@microsoft.com>
2021-05-10 08:54:38 +0200

    First commit - asp.net core app + cloud service calling it separately

---
+++ WorkerRole1/WorkerRole.cs
@@ -0,0 +1,118 @@
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using AspNetCoreWebApi;
+using System.IO;
+
+namespace WorkerRole1
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        //private IWebHost host;
+
+        public override void Run()
+        {
+            Trace.TraceInformation("WorkerRole1 is running");
+
+            try
+            {
+                this.RunAsync(this.cancellationTokenSource.Token).Wait();
+            }
+            finally
+            {
+                this.runCompleteEvent.Set();
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at https://go.microsoft.com/fwlink/?LinkId=166357.
+
+
+            var endpoint = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["Endpoint1"];
+
+
+            string address =  endpoint.IPEndpoint.Address.ToString();
+            var url = $"{endpoint.Protocol}://{address}:{endpoint.IPEndpoint.Port}";
+            var urls = new[] { url };
+
+            Trace.TraceInformation(String.Format("Starting ASP.NET Core at {0}", url), "Information");
+
+            try
+            {
+                /*host = new WebHostBuilder()
+                //.UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                //.UseHttpSys()
+                .UseUrls(urls)
+                .Build();*/
+
+                /*
+                var builder = WebHost.CreateDefaultBuilder(new string[0])
+                .UseStartup<Startup>();
+
+                host = builder.Build();
+                */
+
+                Program.Main(null);
+
+            }
+            catch(Exception exc)
+            {
+                Trace.TraceError(exc.Message);
+
+            }
+
+            /*
+            var host1 = WebHost.CreateDefaultBuilder(null)
+                .UseStartup<Startup>();
+            */
+
+            //string baseUri = String.Format("{0}://{1}", endpoint.Protocol, endpoint.IPEndpoint);
+
+
+            bool result = base.OnStart();
+
+            Trace.TraceInformation("WorkerRole1 has been started");
+
+            return result;
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("WorkerRole1 is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("WorkerRole1 has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following with your own logic.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
