09215beed834f790cf37181d9e05621dddb7f210
OlgaAlekhina <olgaalekhina@microsoft.com>
2021-05-10 10:49:10 +0200

    add home controller

---
+++ AspNetCoreWebApi/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public ActionResult<string> Index()
+        {
+            return "that's home";
+        }
+    }
+}
